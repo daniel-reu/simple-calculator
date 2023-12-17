bba591313a2c4c951ff035bf0e51c6305341c122
Daniel Reuther <d.reuther@hotmail.de>
2023-12-17 22:21:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple calculator
+
+This SvelteKit application presents a simple calculator, with basic operations like addition, subtraction, multiplication, and division.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	
+</script>
+
+<div class="flex flex-col justify-center w-full items-center min-h-screen">
+    
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: #8697d1;
+    }
+</style>
